ab8250259c4ad20fa7fcefb7a7ac1de7c881e09f
polina.voronova <polonium.pv@gmail.com>
2019-10-02 19:20:37 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.result = null;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.result = Math.ceil((this.max + this.min) / 2);
+      return this.result;
     }
-
+  
     lower() {
-
+      this.max = this.result;
     }
-
+  
     greater() {
-
+      this.min = this.result;
     }
-}
+  }
 
 module.exports = GuessingGame;
